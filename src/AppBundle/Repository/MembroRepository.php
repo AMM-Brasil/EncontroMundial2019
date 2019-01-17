c59710bb11627ca04813f4b16c4898509799b127
Natanael Simoes <natanaelsimoes@gmail.com>
2019-01-17 11:07:10 -0400

    retirada de parametro

--- src/AppBundle/Repository/MembroRepository.php
+++ src/AppBundle/Repository/MembroRepository.php
@@ -4,16 +4,17 @@ namespace AppBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
 
-class MembroRepository extends EntityRepository {
-    
-    public function countByCatreBloco(string $bloco) {
-        $rsm = new \Doctrine\ORM\Query\ResultSetMapping;
+class MembroRepository extends EntityRepository
+{
+    public function countByCatreBloco($bloco)
+    {
+        $rsm = new \Doctrine\ORM\Query\ResultSetMapping();
         $rsm->addScalarResult('inscritos', 'inscritos', 'integer');
+
         return $this->getEntityManager()->createNativeQuery(
-                'SELECT count(*) inscritos FROM membro WHERE estadia = :bloco', 
+                'SELECT count(*) inscritos FROM membro WHERE estadia = :bloco',
                 $rsm)
                 ->setParameter(':bloco', $bloco)
                 ->getSingleScalarResult();
     }
-    
 }
